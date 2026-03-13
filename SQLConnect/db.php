73a47c7afdcd018c777101990abaac68a94934c3
jsanjuan <jessica.sanjuan@ucalgary.ca>
2026-03-13 14:00:48 -0600

    another update for db.php
    Added env variables in the app services to be used. safer to do this way.

--- SQLConnect/db.php
+++ SQLConnect/db.php
@@ -1,10 +1,11 @@
 <?php
-
-	$host = "sustainabilitymazegame.mysql.database.azure.com";
-	$username = "group03";
-	$password = "MswGTMLvM?*x@w7";
-	$db_name = "sustainabilitymazegame";
-	$port = 3306;
+	// Use Azure App Service environment variables if available,
+	// otherwise fall back to hardcoded values for testing.
+	$host = getenv("DB_HOST") ?: "sustainabilitymazegame.mysql.database.azure.com";
+	$username = getenv("DB_USER") ?: "group03";
+	$password = getenv("DB_PASS") ?: "MswGTMLvM?*x@w7";
+	$db_name = getenv("DB_NAME") ?: "sustainabilitymazegame";
+	$port = (int)(getenv("DB_PORT") ?: 3306);
 
 	// certificate file should be inside the deployed app folder
 	$ssl_ca = __DIR__ . "/../DigiCertGlobalRootG2.crt.pem";
@@ -18,7 +19,15 @@
 
 	mysqli_ssl_set($con, null, null, $ssl_ca, null, null);
 
-	if (!mysqli_real_connect($con, $host, $username, $password, $db_name, $port, null, MYSQLI_CLIENT_SSL)) 
+	if (!mysqli_real_connect(
+		$con, 
+		$host, 
+		$username, 
+		$password, 
+		$db_name, 
+		$port, 
+		null, 
+		MYSQLI_CLIENT_SSL)) 
 	{
 		die("1: Failed to connect to server");
 	}
