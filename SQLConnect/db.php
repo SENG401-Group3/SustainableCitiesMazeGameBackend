99dff9b3d5423924b10073d43799c339fd258129
jsanjuan <jessica.sanjuan@ucalgary.ca>
2026-03-13 13:35:17 -0600

    Update db.php file
    Update the following:
    - Added the port to be used
    - added the correct directory of our cert file
    - let's leave the connection open and close later in the code
    - reverse the check

--- SQLConnect/db.php
+++ SQLConnect/db.php
@@ -4,17 +4,24 @@
 	$username = "group03";
 	$password = "MswGTMLvM?*x@w7";
 	$db_name = "sustainabilitymazegame";
+	$port = 3306;
+
+	// certificate file should be inside the deployed app folder
+	$ssl_ca = __DIR__ . "/../DigiCertGlobalRootG2.crt.pem";
 
 	$con = mysqli_init();
-	mysqli_ssl_set($con, null, null, "/var/www/html/DigiCertGlobalRootCA.crt.pem", null, null);
-	mysqli_real_connect($con, $host, $username, $password, $db_name, 3306);
-	if (mysqli_connect_errno($con))
+
+	if (!$con) 
+	{
+		die("1: Failed to initialize MySQL connection");
+	}
+
+	mysqli_ssl_set($con, null, null, $ssl_ca, null, null);
+
+	if (!mysqli_real_connect($con, $host, $username, $password, $db_name, $port, null, MYSQLI_CLIENT_SSL)) 
 	{
 		die("1: Failed to connect to server");
 	}
 
-	echo "0";
-
-	mysqli_close($con);
-
+	mysqli_set_charset($con, "utf8mb4");
 ?>
