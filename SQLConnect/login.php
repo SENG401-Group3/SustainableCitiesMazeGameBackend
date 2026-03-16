c1688d7fdc350928bc06b5d41a6a0f5da9b50ec2
Kevin <kevin.he1@ucalgary.ca>
2026-03-15 20:40:30 -0600

    Modified login.php to also fetch the player's progress
    Corresponding frontend changes can be found in the Fetch-User-Progress branch

--- SQLConnect/login.php
+++ SQLConnect/login.php
@@ -7,7 +7,7 @@
 
     if ($username === "" || $password === "")
     {
-        echo "2: Missing username or password";
+        echo json_encode(["error" => "2: Missing username or password"]);
         //close the connection to the database
         $con->close();
         exit();
@@ -18,7 +18,7 @@
 
     if(!$stmt)
     {
-        echo "3: Failed to prepare statement";
+        echo json_encode(["error" => "3: Failed to prepare statement"]);
         $con->close();
         exit();
     }
@@ -29,7 +29,7 @@
     // Check if a user was found
     if($result->num_rows !== 1)
     {
-        echo "5: No user found.";
+        echo json_encode(["error" => "5: No user found"]);
         $stmt->close();
         $con->close();
         exit();
@@ -38,14 +38,36 @@
     $row = $result->fetch_assoc();
     $storedHash = $row["password"];
 
-    if(password_verify($password, $storedHash))
+    if(!password_verify($password, $storedHash))
     {
-        echo "0";
+        echo json_encode(["error" => "6: Incorrect password"]);
+        $stmt->close();
+        $con->close();
+        exit();
     }
-    else
-    {
-        echo "6: Incorrect password";
+
+    $query = "SELECT id FROM users WHERE username = ?";
+    $stmt = $con->prepare($query);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $userId = $row["id"];
+    
+    $query = "SELECT city, score FROM users WHERE id = ?";
+    $stmt = $con->prepare($query);
+    $stmt->bind_param("i", $userId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows !== 1) {
+        echo json_encode(["error" => "7: User data not found."]);
+        $stmt->close();
+        $con->close();
+        exit();
     }
+    $progress = $result->fetch_assoc();
+
+    echo json_encode($progress);
 
     $stmt->close();
     $con->close();
